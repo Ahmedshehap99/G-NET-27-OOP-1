af17c182eefe1ad17c1a79d9997aa173346cc428
AhmedShehap99 <55rrtyy7@gmail.com>
2026-04-01 06:53:42 +0100

    Question 04

--- OOP Assignment 1/Program.cs
+++ OOP Assignment 1/Program.cs
@@ -86,6 +86,26 @@
 
                 Select the library. */
             #endregion
+            #region Question 04
+
+
+
+            /* Q4: What is a Class Library?
+
+                A Class Library is a collection of reusable classes and methods packaged into a DLL.
+
+                Why use it?
+
+                Code reuse
+
+                Better organization
+
+                Easier maintenance
+
+                Separation of concerns */
+
+            #endregion
+
 
 
 
